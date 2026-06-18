a1b623e17d172ea14dcad1169c09378ba4c90268
gianluiss <gianluistapia28@gmail.com>
2026-06-19 00:56:02 +0800

    Added page layout with grid, card-grid & styles

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const myLibrary = [];
 
-function Book(title, author, isRead) {
+function Book(title, author, genre, isRead) {
     if(!new.target) {
         throw Error("You must use the 'new' keyword to construct");
     }
     this.id = crypto.randomUUID();
     this.title =  title;
     this.author = author;
+    this.genre = genre;
     this.isRead = isRead;
 }
 
-function addBookToLibrary(title, author, isRead) {
-    myLibrary.push(new Book(title, author, isRead));
+function addBookToLibrary(title, author, genre, isRead) {
+    myLibrary.push(new Book(title, author, genre, isRead));
 }
 
 function displayBooks() {
     for(const book of myLibrary) {
-        console.log(`ID: ${book.id} | Title: ${book.title} | Author: ${book.author} | isRead: ${book.isRead}`)
+        console.log(`ID: ${book.id} | Title: ${book.title} | Author: ${book.author} | Genre: ${book.genre} | isRead: ${book.isRead}`)
     }
 }
 
-addBookToLibrary("Dune", "Frank Herbert", false);
-addBookToLibrary("Tiki Tembo", "Michelle Obama", true);
-addBookToLibrary("Interstellar", "Christopher Nolan", true);
+addBookToLibrary("Dune", "Frank Herbert", "Sci-Fi", false);
+addBookToLibrary("Tiki Tembo", "Michelle Obama", "Fantasy", true);
+addBookToLibrary("Interstellar", "Christopher Nolan", "Sci-Fi", true);
 
 displayBooks();
